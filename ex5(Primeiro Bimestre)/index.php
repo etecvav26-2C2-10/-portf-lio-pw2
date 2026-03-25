8e31ae6a61eebfa00c631437e173a6f2a9810e01
Daniel Mizael de M. F. <danielmizaeldemf@gmail.com>
2026-03-25 16:38:10 -0300

    Adicionado o codigo depois de ser testado
    Há alguns erros no código entretanto vou ajeita-los

---
+++ ex5(Primeiro Bimestre)/index.php
@@ -0,0 +1,27 @@
+<!--
+Data15/03/26
+Nome:Daniel Mizael de Mesquita Ferreira
+Objetivo:
+
+Exercício 5 - Somatório de Fatoriais
+Leia 5 números inteiros e mostre a soma dos fatoriais desses números.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <a href="calcular.php"></a>
+        <body>
+            <p> Instruções:<br> Digite os numero em cada caixa de texto e aperte o botão "Calcular" </p>
+            <div>
+                <form action="" method="post">
+                    <input type="number" name="n1">
+                    <input type="number" name="n2">
+                    <input type="number" name="n3">
+                    <input type="number" name="n4">
+                    <input type="number" name="n5">
+                    <button type="$_POST">Calcular</button>
+                </form>
+            </div>
+        </body>
+    </head>
+</html>
