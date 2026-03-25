ceda8a9b919acda7842a35628013cc7b19c03516
Daniel Mizael de M. F. <danielmizaeldemf@gmail.com>
2026-03-25 17:17:03 -0300

    upload do ex6
    Ainda há erros no exercício , mas no momento o código já está formado só falta achar um meio dele reconhecer o processo do "calcular".

---
+++ ex6(Primeiro Bimestre)/calcular.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+    <a href="index.php">Retornar</a>
+    <?php
+        var_dump($_POST);
+        $num = $_POST['num'];
+
+        $x = 0;
+        $y = 1;
+
+        for($i = 0;$i < $num;$i++){
+            echo $x .'';
+            $resultado = $x + $y;
+            $x = $x;
+            $resultado = $y;
+
+        }
+    ?>
+</body>
+</html>
+
+
+
---
+++ ex6(Primeiro Bimestre)/index.php
@@ -0,0 +1,27 @@
+<!--
+Data 22/03/26
+Autor:Daniel Mizael de Mesquita Ferreira
+Objetivo:
+
+Exercicio 6
+Leia um número n e mostre os n primeiros termos da sequência de Fibonacci.
+-->
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+<body>
+    <h1>Sequência de Fibonacci</h1>
+    <p>Instruções:<br> Digite um número n , aperte o botão e a sequência será ate o número n  </p>
+    <div>
+        <form action="calcular.php" method="num">
+            <input type="number" name="num" required>
+            <button type="submit">calcular</button>
+        </form>
+    </div>
+</body>
+
+</html>
