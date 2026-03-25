1b1b302aa0fb14307d63ff80f2a6613ea645e7f1
Daniel Mizael de M. F. <danielmizaeldemf@gmail.com>
2026-03-25 16:41:21 -0300

    Adicionado o codigo depois de ser testado
    No momento ele não esta identificando os imput.

---
+++ ex5(Primeiro Bimestre)/calcular.php
@@ -0,0 +1,25 @@
+<!--
+Data:25/03/26
+Nome:Daniel Mizael de Mesquita Ferreira
+Objetivo:
+
+Exercício 5 - Somatório de Fatoriais
+Leia 5 números inteiros e mostre a soma dos fatoriais desses números.
+-->
+<!DOCTYPE html>
+
+<?php
+
+    $n1 = $_POST['n1'];
+    $n2 = $_POST['n2'];
+    $n3 = $_POST['n3'];
+    $n4 = $_POST['n4'];
+    $n5 = $_POST['n5'];
+
+    $resultado = $n1 + $n2 + $n3 + $n4 + $n5;
+
+    echo $resultado;
+
+
+
+?>
