94f930398ce9d51459d3215ea2cff7c742819146
Jota <utyeras16@gmail.com>
2026-03-28 23:34:18 -0300

    Erro corrigido (nome errado na hora de postar no github)

--- ex7(Primeiro Bimestre)/index.php
+++ ex7(Primeiro Bimestre)/index.php
@@ -2,25 +2,33 @@
 Data 28/03/26
 Autor: João Paulo Marques Pinto
 Objetivo:
-Exercício 8 - Soma de 0 até N com Função
-Crie uma função soma(n) que receba um número inteiro e retorne a soma de 0 até esse número.
+Exercício 7 - Separar Positivos e Negativos
+Leia 8 números inteiros e separe em dois vetores:
+Um vetor com números positivos
+Um vetor com números negativos
 -->
-
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <title>Exercício 8 - Soma de 0 até N</title>
+    <title>Exercício 7 - Positivos e Negativos</title>
 </head>
 <body>
-    <h1>Exercício 8 - Soma de 0 até N</h1>
-    <p>Instruções: Digite um número inteiro e clique em "Somar".</p>
+    <h1>Exercício 7 - Separar Positivos e Negativos</h1>
+    <p>Instruções: Digite 8 números inteiros e clique em "Separar".</p>
 
     <form method="post">
-        <input type="number" name="n" required>
-        <button type="submit">Somar</button>
+        <input type="number" name="n1" required><br>
+        <input type="number" name="n2" required><br>
+        <input type="number" name="n3" required><br>
+        <input type="number" name="n4" required><br>
+        <input type="number" name="n5" required><br>
+        <input type="number" name="n6" required><br>
+        <input type="number" name="n7" required><br>
+        <input type="number" name="n8" required><br><br>
+        <button type="submit">Separar</button>
     </form>
 
-    <?php include 'processa.php'; ?>
+    <?php include 'calcula.php'; ?>
 </body>
 </html>
