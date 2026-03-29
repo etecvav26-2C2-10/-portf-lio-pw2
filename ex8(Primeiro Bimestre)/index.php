294e71000812eb654a33d69f1e60c310f91cc67e
Jota <utyeras16@gmail.com>
2026-03-28 23:30:42 -0300

    Erro corrigido (nome errado na hora de postar no github)

--- ex8(Primeiro Bimestre)/index.php
+++ ex8(Primeiro Bimestre)/index.php
@@ -5,7 +5,6 @@ Objetivo:
 Exercício 8 - Soma de 0 até N com Função
 Crie uma função soma(n) que receba um número inteiro e retorne a soma de 0 até esse número.
 -->
-
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -21,6 +20,6 @@ Crie uma função soma(n) que receba um número inteiro e retorne a soma de 0 at
         <button type="submit">Somar</button>
     </form>
 
-    <?php include 'processa.php'; ?>
+    <?php include 'calcula.php'; ?>
 </body>
 </html>
