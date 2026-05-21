a8f37c56b45cea2ffb45904f88658c9ae27a56ca
Jota <utyeras16@gmail.com>
2026-05-20 22:25:34 -0300

    Adicionada estrutura inicial do banco de dados farmacia_vav
    .

---
+++ 2BIM/database.sql
@@ -0,0 +1,11 @@
+-- ============================================
+-- Projeto Escolar: CRUD Farmácia VAV
+-- Arquivo: database.sql
+-- ============================================
+
+-- Cria o banco se não existir
+CREATE DATABASE IF NOT EXISTS farmacia_vav
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+USE farmacia_vav;
