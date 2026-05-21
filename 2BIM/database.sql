4fa3dcdd7dc119b102587b98ab51617e64d58c80
Jota <utyeras16@gmail.com>
2026-05-20 22:35:09 -0300

    Criada tabela produtos com estrutura principal do sistema
    .

--- 2BIM/database.sql
+++ 2BIM/database.sql
@@ -12,3 +12,14 @@ USE farmacia_vav;
 
 -- Remove e recria a tabela
 DROP TABLE IF EXISTS produtos;
+
+CREATE TABLE produtos (
+    id          INT(11)        NOT NULL AUTO_INCREMENT,
+    nome        VARCHAR(150)   NOT NULL,
+    fabricante  VARCHAR(100)   NOT NULL,
+    preco       DECIMAL(10,2)  NOT NULL DEFAULT 0.00,
+    estoque     INT(11)        NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB
+  DEFAULT CHARSET=utf8mb4
+  COLLATE=utf8mb4_unicode_ci;
