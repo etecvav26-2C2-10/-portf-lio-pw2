547529e3b1c43736c5fc7611d334cd0840dad7e4
Jota <utyeras16@gmail.com>
2026-05-20 21:50:54 -0300

    Definidas constantes de configuração do banco de dados
    .

---
+++ 2BIM/config/conexao.example.php
@@ -0,0 +1,10 @@
+<?php
+// ============================================
+// Projeto Escolar: CRUD Farmácia VAV
+// Arquivo: config/conexao.example.php
+//
+define('DB_HOST',    'localhost');
+define('DB_NAME',    'farmacia_vav');
+define('DB_USER',    'seu_usuario');   // ex: root
+define('DB_PASS',    'sua_senha');     // ex: 1234
+define('DB_CHARSET', 'utf8mb4');
